a5dac22cd3f13b3eec5088498bfde028afe55d0f
alejandro lopez lugris <you@example.com>
2024-11-06 09:43:07 +0100

    cors

--- src/main/java/alexlus/maulasfc/controller/PersonaController.java
+++ src/main/java/alexlus/maulasfc/controller/PersonaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin("https://mls-fc-production.up.railway.app")
 @RestController
 @RequestMapping("/persona")
 @RequiredArgsConstructor
